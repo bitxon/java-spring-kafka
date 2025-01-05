56735f46af12ce8ecb952c0826f7fff1609a02fe
Nikita Karpuk <bitxon.dev@gmail.com>
2025-01-05 17:31:46 +0100

    Spring Boot '3.4.1'

--- kafka-batch-pojo-v2/src/test/java/bitxon/spring/kafka/config/TestContainersConfig.java
+++ kafka-batch-pojo-v2/src/test/java/bitxon/spring/kafka/config/TestContainersConfig.java
@@ -4,7 +4,7 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertyRegistrar;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -12,11 +12,14 @@ import org.testcontainers.utility.DockerImageName;
 public class TestContainersConfig {
 
     @Bean
+    public DynamicPropertyRegistrar propertiesOverride(KafkaContainer kafka) {
+        return (registry) -> registry.add("custom.shipment.bootstrap-servers", kafka::getBootstrapServers);
+    }
+
+    @Bean
     @ServiceConnection
-    public KafkaContainer kafkaContainer(DynamicPropertyRegistry dynamicPropertyRegistry) {
-        var container = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.2.1"));
-        dynamicPropertyRegistry.add("custom.shipment.bootstrap-servers", container::getBootstrapServers);
-        return container;
+    public KafkaContainer kafkaContainer() {
+        return new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.2.1"));
     }
 
     @Bean
--- kafka-batch-pojo/src/test/java/bitxon/spring/kafka/config/TestContainersConfig.java
+++ kafka-batch-pojo/src/test/java/bitxon/spring/kafka/config/TestContainersConfig.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistrar;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
@@ -12,11 +13,14 @@ import org.testcontainers.utility.DockerImageName;
 public class TestContainersConfig {
 
     @Bean
+    public DynamicPropertyRegistrar propertiesOverride(KafkaContainer kafka) {
+        return (registry) -> registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+    }
+
+    @Bean
     @ServiceConnection
-    public KafkaContainer kafkaContainer(DynamicPropertyRegistry dynamicPropertyRegistry) {
-        var container = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.2.1"));
-        dynamicPropertyRegistry.add("spring.kafka.bootstrap-servers", container::getBootstrapServers);
-        return container;
+    public KafkaContainer kafkaContainer() {
+        return new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.2.1"));
     }
 
     @Bean
